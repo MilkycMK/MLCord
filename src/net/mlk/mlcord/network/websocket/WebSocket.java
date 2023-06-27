c9f6fe6d2ea29f00382eaedbd7fc6a4c130957fc
milkyc <sagaidaan@yandex.ru>
2023-06-27 18:26:28 +0300

    remove unused import

--- src/net/mlk/mlcord/network/websocket/WebSocket.java
+++ src/net/mlk/mlcord/network/websocket/WebSocket.java
@@ -18,7 +18,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Random;
-import java.util.concurrent.*;
 
 public class WebSocket {
     private URI uri;
